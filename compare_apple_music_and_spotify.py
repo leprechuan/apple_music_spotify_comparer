966cf8c8b85ccae0e52ce4fb28422ce60a97dff3
leprechaun <fd>
2020-02-07 17:37:19 +0100

    apple music part done

--- compare_apple_music_and_spotify.py
+++ compare_apple_music_and_spotify.py
@@ -1,13 +1,15 @@
 import io
+import re
 
-def main():
-    print("Hello World!")
 
-class apple_music_data_reader():
+
+class apple_music_data_parser():
 
     def __init__(self,file_name):
         self.file_name = file_name
-        self.file_data =self.open_file()
+        self.file_data = self.open_file()
+        self.one_song_and_artist = {}
+        self.all_songs_and_artists = []
 
 
     def open_file(self):
@@ -17,21 +19,47 @@ class apple_music_data_reader():
 
     def read_line_by_line(self):
         data=""
-
         buf = io.StringIO(self.file_data)
         for a in buf:
             data =data + a.strip()
         return data
 
-    def read_artist(self, line):
+    def is_artist(self, line):
         if "Artist" in line:
-            return "Drew Goddard"
+            return re.search(r'<string>(.*?)</string', line).group(1)
 
-
-    def read_song(self, line):
+    def is_song(self, line):
         if "Name" in line:
-            return "Sounds of a Playground Fading"
+            return re.search(r'<string>(.*?)</string', line).group(1)
 
-if __name__ == "__main__":
-    main()
+    def save_song(self,Name):
+        self.one_song_and_artist.update({'Song':Name})
+
+    def save_if_artist(self,line):
+        artist_name = self.is_artist(line)
+        if artist_name:
+            self.one_song_and_artist.update({'Artist':artist_name})
+
+    def save_if_song(self,line):
+        song = self.is_song(line)
+        if song:
+            self.one_song_and_artist.update({'Song':song})
+
+    def save_song_and_artist(self):
+        self.all_songs_and_artists.append(self.one_song_and_artist)
+        self.one_song_and_artist={}
+
+    def create(self):
+        data = ""
+        buf = io.StringIO(self.file_data)
+        for a in buf:
+            self.save_if_artist(a)
+            self.save_if_song(a)
+            if 'Artist' in self.one_song_and_artist and 'Song' in self.one_song_and_artist:
+                self.save_song_and_artist()
+
+        return self.all_songs_and_artists
+
+
+
 
--- test_compare_apple_music_and_spotify.py
+++ test_compare_apple_music_and_spotify.py
@@ -11,41 +11,59 @@ class get_apple_music_data(unittest.TestCase):
     DATA_SEVERAL_LINES = dedent("""<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">""")
 
+    DATA_ONE_ARTIST = dedent("""<key>Artist</key><string>Drew Goddard</string>""")
+    DATA_ONE_SONG = dedent("""<key>Name</key><string>The Cabin In the Woods</string>""")
+    DATA_ONE_SONG_AND_ARTIST = dedent("""<key>Artist</key><string>Drew Goddard</string>
+                                        <key>Name</key><string>The Cabin In the Woods</string>""")
+
+    DATA_SEVERAL_ARTISTS_AND_SONGS = dedent('''<key>Name</key><string>The Cabin In the Woods</string>
+    <key>Artist</key><string>Drew Goddard</string>
+    <key>Name</key><string>Pulp Fiction</string>
+	<key>Artist</key><string>Quentin Tarantino</string>''')
 
     def setUp(self):
-        self.apple_music_data_reader = music_compare.apple_music_data_reader("apple_music_library.xml")
+        self.apple_music_data_parser = music_compare.apple_music_data_parser("apple_music_library.xml")
 
     @patch("builtins.open", mock_open(read_data=DATA_ONE_LINE))
     def test_open_file(self):
-        result = self.apple_music_data_reader.open_file()
+        result = self.apple_music_data_parser.open_file()
         open.assert_called_once_with("apple_music_library.xml", "r")
         self.assertEqual("""<?xml version="1.0" encoding="UTF-8"?>""", result)
 
     @patch("builtins.open", mock_open(read_data=DATA_ONE_LINE))
-    def test_one_line(self):
-        self.apple_music_data_reader = music_compare.apple_music_data_reader("apple_music_library.xml")
-        self.assertEqual('''<?xml version="1.0" encoding="UTF-8"?>''',self.apple_music_data_reader.read_line_by_line())
+    def test_read_one_line(self):
+        self.apple_music_data_parser = music_compare.apple_music_data_parser("apple_music_library.xml")
+        self.assertEqual('''<?xml version="1.0" encoding="UTF-8"?>''',self.apple_music_data_parser.read_line_by_line())
 
     @patch("builtins.open", mock_open(read_data=DATA_SEVERAL_LINES))
-    def test_several_lines(self):
-        self.apple_music_data_reader = music_compare.apple_music_data_reader("apple_music_library.xml")
-        self.assertEqual('''<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">''',self.apple_music_data_reader.read_line_by_line())
+    def test_read_several_lines(self):
+        self.apple_music_data_parser = music_compare.apple_music_data_parser("apple_music_library.xml")
+        self.assertEqual('''<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">''',self.apple_music_data_parser.read_line_by_line())
 
-    def test_read_one_artist(self):
-        self.apple_music_data_reader = music_compare.apple_music_data_reader("apple_music_library.xml")
-        self.assertEqual('''Drew Goddard''',self.apple_music_data_reader.read_artist("""<key>Artist</key><string>Drew Goddard</string>"""))
+    @patch("builtins.open", mock_open(read_data=DATA_ONE_ARTIST))
+    def test_save_one_artist_from_line(self):
+        self.apple_music_data_parser = music_compare.apple_music_data_parser("apple_music_library.xml")
+        self.apple_music_data_parser.create()
+        self.assertEqual("Drew Goddard",self.apple_music_data_parser.one_song_and_artist.get('Artist'))
 
+    @patch("builtins.open", mock_open(read_data=DATA_ONE_SONG))
+    def test_save_one_song(self):
+        self.apple_music_data_parser = music_compare.apple_music_data_parser("apple_music_library.xml")
+        self.apple_music_data_parser.create()
+        self.assertEqual("The Cabin In the Woods", self.apple_music_data_parser.one_song_and_artist.get('Song'))
 
-    def test_read_line_without_artist(self):
-        self.apple_music_data_reader = music_compare.apple_music_data_reader("apple_music_library.xml")
-        self.assertEqual(None,self.apple_music_data_reader.read_artist('''<?xml version="1.0" encoding="UTF-8"?>'''))
+    @patch("builtins.open", mock_open(read_data=DATA_ONE_SONG_AND_ARTIST))
+    def test_save_one_song_and_artist(self):
+        self.apple_music_data_parser = music_compare.apple_music_data_parser("apple_music_library.xml")
+        self.apple_music_data_parser.create()
+        self.assertEqual([{'Artist': "Drew Goddard",'Song':"The Cabin In the Woods"}],self.apple_music_data_parser.all_songs_and_artists)
 
-    def test_read_line_one_song(self):
-        self.apple_music_data_reader = music_compare.apple_music_data_reader("apple_music_library.xml")
-        self.assertEqual('''Sounds of a Playground Fading''',self.apple_music_data_reader.read_song("""<key>Name</key><string>Sounds of a Playground Fading</string>"""))
+    @patch("builtins.open", mock_open(read_data=DATA_SEVERAL_ARTISTS_AND_SONGS))
+    def test_save_several_songs_and_artists(self):
+        self.apple_music_data_parser = music_compare.apple_music_data_parser("apple_music_library.xml")
+        self.apple_music_data_parser.create()
+        self.assertEqual([{'Artist': "Drew Goddard",'Song':"The Cabin In the Woods"},{'Artist': "Quentin Tarantino",'Song':"Pulp Fiction"}],self.apple_music_data_parser.all_songs_and_artists)
 
+    #def test_lines_no_artist_or_Song(self):
 
-    def test_read_line_withoput_song(self):
-        self.apple_music_data_reader = music_compare.apple_music_data_reader("apple_music_library.xml")
-        self.assertEqual(None,self.apple_music_data_reader.read_song('''<?xml version="1.0" encoding="UTF-8"?>'''))
 
