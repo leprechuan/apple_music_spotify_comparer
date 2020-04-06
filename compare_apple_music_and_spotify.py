8105bc01788747b373c90dbfc642c0179ffdcf7e
tobias <f>
2020-04-06 20:52:41 +0200

    spotify parser done

--- compare_apple_music_and_spotify.py
+++ compare_apple_music_and_spotify.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import io
 import re
 import csv
@@ -15,12 +17,6 @@ class apple_music_data_parser():
         with open(self.file_name, "r") as f:
             return f.read()
 
-    def read_line_by_line(self):
-        data=""
-        buf = io.StringIO(self.file_data)
-        for a in buf:
-            data =data + a.strip()
-        return data
 
     def is_artist(self, line):
         if "Artist" in line:
@@ -63,10 +59,37 @@ class apple_music_data_parser():
 
 class spotify_data_parser():
 
-    def __init__(self,file_name):
-        self.data=self.read_file(file_name)
+    def __init__(self):
+        self.all_songs_and_artists = []
+        self.one_song_and_artist = {}
 
-    def read_file(self,file_name):
-        pass
-        with open(file_name, "r" ) as csvfile:
-            return csvfile.read()
+    def read_file(self,csvfile):
+        with open(csvfile, "r" , newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            return reader
+
+    def is_artist(self,data):
+        return "Artist Name" in data
+
+    def is_song(self,data):
+        return "Track Name" in data
+
+    def save_artist(self,data):
+       if self.is_artist(data):
+           self.one_song_and_artist['Artist'] = data['Artist Name']
+
+    def save_song(self,data):
+       if self.is_song(data):
+           self.one_song_and_artist['Song']=data['Track Name']
+
+    def combine_song_and_artist(self):
+        if self.one_song_and_artist.get('Song') != None and self.one_song_and_artist.get('Artist') != None:
+            self.all_songs_and_artists.append(self.one_song_and_artist)
+
+    def create(self, file_name):
+        csv_formated_data=self.read_file(file_name)
+        for row in csv_formated_data:
+            self.save_song(row)
+            self.save_artist(row)
+            self.combine_song_and_artist()
+            self.one_song_and_artist = {}
--- test_compare_apple_music_and_spotify.py
+++ test_compare_apple_music_and_spotify.py
@@ -32,10 +32,6 @@ class get_apple_music_data(unittest.TestCase):
         result = self.apple_music_data_parser.file_data
         open.assert_called_once_with(self.file , "r")
         self.assertEqual("""<?xml version="1.0" encoding="UTF-8"?>""", result)
-    @patch("builtins.open", mock_open(read_data=DATA_SEVERAL_LINES))
-    def test_read_several_lines(self):
-        self.apple_music_data_parser = music_compare.apple_music_data_parser(self.file)
-        self.assertEqual('''<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">''',self.apple_music_data_parser.read_line_by_line())
 
     @patch("builtins.open", mock_open(read_data=DATA_ONE_ARTIST))
     def test_save_one_artist_from_line(self):
@@ -61,19 +57,107 @@ class get_apple_music_data(unittest.TestCase):
         self.apple_music_data_parser.create()
         self.assertEqual([{'Artist': "Drew Goddard",'Song':"The Cabin In the Woods"},{'Artist': "Quentin Tarantino",'Song':"Pulp Fiction"}],self.apple_music_data_parser.all_songs_and_artists)
 
-    #def test_lines_no_artist_or_Song(self):
 
 
 class spotify_data_parser(unittest.TestCase):
     DATA_ONE_LINE = dedent("""﻿Spotify URI,Track Name,Artist Name,Album Name,Disc Number,Track Number,Track Duration (ms),Added By,Added At""")
 
-    def setUp(self):
-        self.file = tempfile.NamedTemporaryFile(mode='w', delete=True)
+    LINE_NO_ARTIST = dedent("""<key>Artist</key><string>Drew Goddard</string>""")
+
+    def test_open_file_and_return_formated_data_split_by_coma(self):
+        with patch("builtins.open", mock_open(read_data="split,by,")) as mock_file:
+            result = music_compare.spotify_data_parser().read_file("/test_path")
+            open.assert_called_once_with("/test_path", "r", newline='')
+            self.assertTrue(result,"_csv.DictReader")
+
+    def test_no_artist_found_on_line(self):
+        lines_csv_dict_reader_formated = {
+            "not found": "not important",
+        }
+        result= music_compare.spotify_data_parser().is_artist(lines_csv_dict_reader_formated)
+        self.assertEqual(False,result)
+
+    def test_artist_found_on_line(self):
+        lines_csv_dict_reader_formated = {
+            "Artist": "Avenged Sevenfold",
+        }
+        result= music_compare.spotify_data_parser().is_artist(lines_csv_dict_reader_formated)
+        self.assertEqual(True,result)
+
+    def test_song_not_found_on_line(self):
+        lines_csv_dict_reader_formated = {
+            "not found": "Nightmare",
+        }
+        result= music_compare.spotify_data_parser().is_song(lines_csv_dict_reader_formated)
+        self.assertEqual(False,result)
+
+    def test_song_found_on_line(self):
+        lines_csv_dict_reader_formated = {
+            "Track Name": "Nightmare",
+        }
+        result= music_compare.spotify_data_parser().is_song(lines_csv_dict_reader_formated)
+        self.assertEqual(True,result)
+
+    def test_dont_save_if_artist_not_found(self):
+        lines_csv_dict_reader_formated = {
+            "not found": "not important",
+        }
+        music_compare.spotify_data_parser().save_artist(lines_csv_dict_reader_formated)
+        self.assertEqual({},music_compare.spotify_data_parser().one_song_and_artist)
+
+    def test_save_if_artist_found(self):
+        lines_csv_dict_reader_formated = {
+            "Artist Name": "test_artist",
+        }
+        self.spotify_data_parser = music_compare.spotify_data_parser()
+        self.spotify_data_parser.save_artist(lines_csv_dict_reader_formated)
+        self.assertEqual('test_artist', self.spotify_data_parser.one_song_and_artist.get('Artist'))
 
 
-    @patch("builtins.open", mock_open(read_data=DATA_ONE_LINE))
-    def test_open_file(self):
-        result = self.spotify_data_parser = music_compare.spotify_data_parser(self.file).data
-        open.assert_called_once_with(self.file, "r")
-        self.assertEqual("""﻿Spotify URI,Track Name,Artist Name,Album Name,Disc Number,Track Number,Track Duration (ms),Added By,Added At""", result)
+    def test_dont_save_if_song_not_found(self):
+        lines_csv_dict_reader_formated = {
+            "not found": "not important",
+        }
+        music_compare.spotify_data_parser().save_song(lines_csv_dict_reader_formated)
+        self.assertEqual({},music_compare.spotify_data_parser().one_song_and_artist)
 
+    def test_save_if_song_found(self):
+        lines_csv_dict_reader_formated = {
+            "Track Name": "test_song",
+        }
+        self.spotify_data_parser = music_compare.spotify_data_parser()
+        self.spotify_data_parser.save_song(lines_csv_dict_reader_formated)
+        self.assertEqual('test_song', self.spotify_data_parser .one_song_and_artist.get('Song'))
+
+    def test_combine_song_found_and_NOT_artist(self):
+        lines_csv_dict_reader_formated = {
+            "Name": "test_song",
+            "Artist": "test_artist"
+        }
+        self.spotify_data_parser = music_compare.spotify_data_parser()
+        self.spotify_data_parser.save_song(lines_csv_dict_reader_formated)
+
+        self.spotify_data_parser.combine_song_and_artist()
+        self.assertEqual([], self.spotify_data_parser.all_songs_and_artists)
+
+    def test_combine_song_and_artist_if_found(self):
+        lines_csv_dict_reader_formated = {
+            "Track Name": "test_song",
+            "Artist Name": "test_artist"
+        }
+        self.spotify_data_parser = music_compare.spotify_data_parser()
+        self.spotify_data_parser.save_song(lines_csv_dict_reader_formated)
+        self.spotify_data_parser.save_artist(lines_csv_dict_reader_formated)
+
+        self.spotify_data_parser.combine_song_and_artist()
+        self.assertEqual([{'Artist': 'test_artist', 'Song': 'test_song'}], self.spotify_data_parser.all_songs_and_artists)
+
+
+    def test_combine_several_songs_and_artists(self):
+        with patch("builtins.open", mock_open(read_data='''Spotify URI,Track Name,Artist Name,Album Name,Disc Number,Track Number,Track Duration (ms),Added By,Added At
+"spotify:track:4UEo1b0wWrtHMC8bVqPiH8","Nightmare","Avenged Sevenfold","Nightmare","1","1","374453","spotify:user:","2010-10-17T20:18:40Z"
+"spotify:track:1d5UuboIPRMD4HaU3yycKC","Somewhere I Belong","Linkin Park","Meteora (Bonus Edition)","1","3","213933","spotify:user:","2010-10-17T20:24:25Z"''')) as mock_file:
+            self.spotify_data_parser = music_compare.spotify_data_parser()
+            self.spotify_data_parser.create("/test_path")
+        #self.spotify_data_parser.combine_song_and_artist()
+        self.assertEqual([{'Artist': 'Avenged Sevenfold', 'Song': 'Nightmare'},{'Artist': 'Linkin Park', 'Song': 'Somewhere I Belong'}], self.spotify_data_parser.all_songs_and_artists)
